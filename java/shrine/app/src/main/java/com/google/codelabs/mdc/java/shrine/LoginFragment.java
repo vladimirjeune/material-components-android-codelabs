63baad4f4bdf6888a46a454ef5ef428d7095026d
vladimirjeune <randyjeune@gmail.com>
2018-12-28 17:44:11 -0500

    feat: Finish last part of section MDC-101
    Add Error code from the TextInputLayout. Also have listeners for the button
    press for "Next", and for the Edit Text. So, if the user hits next and sets
    an error; adding more characters(8) will clear the error.

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -2,7 +2,13 @@ package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +23,35 @@ public class LoginFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.shr_login_fragment, container, false);
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        MaterialButton nextButton = view.findViewById(R.id.next_button);
+
+        // Set Error if Text Length is < 8 chars
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (! isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(getString(R.string.shr_error_password));
+                }
+                else
+                {
+                    passwordTextInput.setError(null);
+                    ((NavigationHost)getActivity()).navigateTo(new ProductGridFragment(), false); // Navigate to next Fragment
+                }
+            }
+        });
+
+        // Clear Error once more than 8 chars typed
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(null);  // Clear Error
+                }
+                return false;
+            }
+        });
 
         // Snippet from "Navigate to the next Fragment" section goes here.
 
@@ -24,4 +59,9 @@ public class LoginFragment extends Fragment {
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private boolean isPasswordValid(@Nullable Editable editable) {
+        return ((editable != null) && (editable.length() >= 8));
+    }
+
+
 }
